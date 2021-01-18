68a92cdc7ee45c2542e1207bfc29d1920e450692
Sir Chopwood <sirchopwood@gmail.com>
2021-01-18 16:06:35 +0000

    Mass name setting

--- commands/setname.py
+++ commands/setname.py
@@ -18,30 +18,39 @@ async def set_name(self, ranks, message, user_id):
     elif "$NN" in name:
         await message.channel.send(content="", embed=embedtemplates.failure("Missing Nickname",
                                                                             str("User needs to set a nickname first.")))
-        return
+        return 0
 
     try:
         discorduser = await message.guild.fetch_member(user_id)
         await discorduser.edit(nick=name)
-        await message.channel.send(content="", embed=embedtemplates.success("Name Set", name))
+        return 1
     except discord.Forbidden:
         await message.channel.send(content="", embed=embedtemplates.failure("Missing Permissions",
                                                                             str("I do not have permissions to set your name, " + name)))
-        return
+        return 0
 
 
 async def Main(self, message, command, arguments):
     ranks = self.database.get_ranks(message.guild.id)
     ranks = ranks["Dictionary"]
+    status = 0
     if len(message.mentions) > 0:
         if not await permissions.is_guild_admin(self, message.guild.id, message.author.id):
             await message.channel.send(content="", embed=embedtemplates.failure("Permission Denied",
                                                                                 "User does not have permission to use this!"))
             return
         for member in message.mentions:
-            await set_name(self, ranks, message, member.id)
-        return
+            status += await set_name(self, ranks, message, member.id)
+
+    if len(message.role_mentions) > 0:
+        if not await permissions.is_guild_admin(self, message.guild.id, message.author.id):
+            await message.channel.send(content="", embed=embedtemplates.failure("Permission Denied",
+                                                                                "User does not have permission to use this!"))
+            return
+        for member in message.role_mentions.members:
+            status += await set_name(self, ranks, message, member.id)
 
     else:
-        await set_name(self, ranks, message, message.author.id)
-        return
+        status = await set_name(self, ranks, message, message.author.id)
+
+    await message.channel.send(content="", embed=embedtemplates.success("Name(s) Set", str(str(status) + " Name(s) were set.")))

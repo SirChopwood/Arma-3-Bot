303a9a5024d14829531c52c730e0e9e8a5bc7a21
Sir Chopwood <sirchopwood@gmail.com>
2021-01-23 14:31:13 +0000

    Fixed LOA Header announcement check bug

--- reactions/loa_header.py
+++ reactions/loa_header.py
@@ -72,7 +72,7 @@ async def Main(self, channel, message, user, emoji):
 
             for announcement in self.database.get_all_announcements(message.guild.id):
                 now = datetime.datetime.now()
-                optime = datetime.datetime.strptime(str(announcement["Operation"]["Date"] + "/" + now.strftime("%Y") + " " + announcement["Operation"]["Time"]), '%d/%m/%Y %H:%M')
+                optime = datetime.datetime.strptime(str(announcement["Operation"]["Date"] + " " + announcement["Operation"]["Time"]), '%d/%m/%Y %H:%M')
                 if optime > now:
                     self.announcement_queue.put({"GuildID": message.guild.id, "AnnouncementID": announcement["MessageID"],
                                                  "UserID": user.id, "Status": "Maybe"})
